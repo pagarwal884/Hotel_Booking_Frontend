ba8c2ca62cc052475a496c431f2a32fe9e5331a0
pagarwal884 <pagarwal1145@gmail.com>
2026-07-21 19:21:21 +0530

    Added Exclusive Offer Section in my web page

--- src/components/ExclusiveOffer.jsx
+++ src/components/ExclusiveOffer.jsx
@@ -13,13 +13,19 @@ const ExclusiveOffer = () => {
                 </button>
 
             </div>
-            <div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12'>
                 {exclusiveOffers.map((item) => (
                     <div key={item._id} className='group relative flex flex-col items-start justify-between gap-1 pt-12 md:pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center' style={{backgroundImage: `url(${item.image})`}}>
                         <p className='px-3 py-1 absolute top-4 left-4 text-xs bg-white text-gray-800 font-medium rounded-full'>{item.priceOff}% OFF</p>
                         <div>
-                            <p>{item.title}</p>
+                            <p className='text-2xl font-medium font-playfair'>{item.title}</p>
+                            <p>{item.description}</p>
+                            <p className='text-xs text-white/70 mt-3'>Expire {item.expiryDate}</p>
                         </div>
+                        <button className='flex items-center gap-2 font-medium cursor-pointer mt-4 mb-5'>
+                            View Offer
+                            <img src={assets.arrowIcon} alt="" className='invert group-hover:translate-x-1 transition-all'/>
+                        </button>
 
                     </div>
                 ))}
